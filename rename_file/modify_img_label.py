08103d5d0ad3d44fe5b046c444b5705d1f52117c
laughing429 <327983097@qq.com>
2018-04-26 09:33:09 +0800

    modify

--- rename_file/modify_img_label.py
+++ rename_file/modify_img_label.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from PyQt4 import QtCore, QtGui
+from PyQt5 import QtCore, QtGui, QtWidgets
 import sys, os
 import random
 
@@ -13,12 +13,12 @@ except AttributeError:
 try:
     _encoding = QtGui.QApplication.UnicodeUTF8
     def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+        return QtWidgets.QApplication.translate(context, text, disambig, _encoding)
 except AttributeError:
     def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig)
+        return QtWidgets.QApplication.translate(context, text, disambig)
 
-class Ui_MainWindow(QtGui.QMainWindow):
+class Ui_MainWindow(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         super(Ui_MainWindow, self).__init__()
         self.setupUi(self)
@@ -29,10 +29,10 @@ class Ui_MainWindow(QtGui.QMainWindow):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName(_fromUtf8("modify file name"))
         MainWindow.resize(800, 432)
-        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
         
-        self.path_label = QtGui.QLabel(self.centralwidget)
+        self.path_label = QtWidgets.QLabel(self.centralwidget)
         self.path_label.setGeometry(QtCore.QRect(70, 40, 51, 21))
         font = QtGui.QFont()
         font.setFamily(_fromUtf8("Yu Gothic UI Semibold"))
@@ -42,7 +42,7 @@ class Ui_MainWindow(QtGui.QMainWindow):
         self.path_label.setFont(font)
         self.path_label.setObjectName(_fromUtf8("path_label"))
         
-        self.path_text = QtGui.QTextEdit(self.centralwidget)
+        self.path_text = QtWidgets.QTextEdit(self.centralwidget)
         self.path_text.setGeometry(QtCore.QRect(140, 40, 531, 31))
         font = QtGui.QFont()
         font.setFamily(_fromUtf8("Sim sun"))
@@ -52,7 +52,7 @@ class Ui_MainWindow(QtGui.QMainWindow):
         self.path_text.setFont(font)
         self.path_text.setObjectName(_fromUtf8("path_text"))
 
-        self.img_label = QtGui.QLabel(self.centralwidget)
+        self.img_label = QtWidgets.QLabel(self.centralwidget)
         self.img_label.setGeometry(QtCore.QRect(70, 80, 71, 31))
         font = QtGui.QFont()
         font.setFamily(_fromUtf8("Yu Gothic UI Semibold"))
@@ -62,11 +62,11 @@ class Ui_MainWindow(QtGui.QMainWindow):
         self.img_label.setFont(font)
         self.img_label.setObjectName(_fromUtf8("img_label"))
         
-        self.img = QtGui.QLabel(self.centralwidget)
+        self.img = QtWidgets.QLabel(self.centralwidget)
         self.img.setGeometry(QtCore.QRect(140, 40, 531, 161))
         self.img.setObjectName(_fromUtf8("img"))
         
-        self.ok = QtGui.QPushButton(self.centralwidget)
+        self.ok = QtWidgets.QPushButton(self.centralwidget)
         self.ok.setGeometry(QtCore.QRect(440, 350, 101, 41))
         font = QtGui.QFont()
         font.setFamily(_fromUtf8("Yu Gothic UI Semibold"))
@@ -76,7 +76,7 @@ class Ui_MainWindow(QtGui.QMainWindow):
         self.ok.setFont(font)
         self.ok.setObjectName(_fromUtf8("ok"))
         
-        self.name = QtGui.QTextEdit(self.centralwidget)
+        self.name = QtWidgets.QTextEdit(self.centralwidget)
         self.name.setGeometry(QtCore.QRect(140, 260, 531, 71))
         font = QtGui.QFont()
         font.setFamily(_fromUtf8("Yu Gothic UI Semibold"))
@@ -86,7 +86,7 @@ class Ui_MainWindow(QtGui.QMainWindow):
         self.name.setFont(font)
         self.name.setObjectName(_fromUtf8("name"))
         
-        self.img_label_2 = QtGui.QLabel(self.centralwidget)
+        self.img_label_2 = QtWidgets.QLabel(self.centralwidget)
         self.img_label_2.setGeometry(QtCore.QRect(60, 260, 81, 31))
         font = QtGui.QFont()
         font.setFamily(_fromUtf8("Yu Gothic UI Semibold"))
@@ -96,7 +96,7 @@ class Ui_MainWindow(QtGui.QMainWindow):
         self.img_label_2.setFont(font)
         self.img_label_2.setObjectName(_fromUtf8("img_label_2"))
         
-        self.change = QtGui.QPushButton(self.centralwidget)
+        self.change = QtWidgets.QPushButton(self.centralwidget)
         self.change.setGeometry(QtCore.QRect(260, 350, 101, 41))
         font = QtGui.QFont()
         font.setFamily(_fromUtf8("Yu Gothic UI Semibold"))
@@ -107,7 +107,7 @@ class Ui_MainWindow(QtGui.QMainWindow):
         self.change.setObjectName(_fromUtf8("change"))
         
         MainWindow.setCentralWidget(self.centralwidget)
-        self.statusbar = QtGui.QStatusBar(MainWindow)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName(_fromUtf8("statusbar"))
         MainWindow.setStatusBar(self.statusbar)
 
@@ -130,7 +130,7 @@ class Ui_MainWindow(QtGui.QMainWindow):
         self.path = str(path)
         pics = os.listdir(self.path)
         n = len(pics)
-        num = random.randint(0, n)
+        num = random.randint(0, n) # 随机取
         self.pic = pics[num]
         with open(os.path.join(self.path, self.pic), 'rb') as f:
             self.image = f.read()
@@ -147,7 +147,7 @@ class Ui_MainWindow(QtGui.QMainWindow):
         self.change_picture()
 
 if __name__ == '__main__':
-    app = QtGui.QApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)
     modify = Ui_MainWindow()
     modify.show()
     app.exec_()
